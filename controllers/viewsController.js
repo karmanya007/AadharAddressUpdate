459d8f92a739b382fc6d4df594fdea4d10caec20
Aditya-011 <adihgr18@gmail.com>
2021-10-29 13:27:40 +0530

    Catpcha added

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,3 +1,5 @@
+const { v4: uuidv4 } = require('uuid');
 exports.getOverview = async (req, res, next) => {
+	const reqId = uuidv4();
 	res.status(200).render('index');
 };
---
+++ public/app.js
@@ -0,0 +1,77 @@
+//console.log(1010);
+var otp;
+const createCaptcha = () => {
+  axios
+    .post(
+      "https://stage1.uidai.gov.in/unifiedAppAuthService/api/v2/get/captcha",
+      { "Content-Type": "application/json " }
+    )
+    .then(function (response) {
+     // console.log(response.data.captchaBase64String);
+      let str = response.data.captchaBase64String;
+      let blob = atob(str);
+      //console.log(blob);
+      const byteNumbers = new Array(blob.length);
+      for (let i = 0; i < blob.length; i++) {
+        byteNumbers[i] = blob.charCodeAt(i);
+      }
+      const byteArray = new Uint8Array(byteNumbers);
+      const b64toBlob = (str, contentType = "image/png", sliceSize = 512) => {
+        const byteCharacters = atob(str);
+        const byteArrays = [];
+
+        for (
+          let offset = 0;
+          offset < byteCharacters.length;
+          offset += sliceSize
+        ) {
+          const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+          const byteNumbers = new Array(slice.length);
+          for (let i = 0; i < slice.length; i++) {
+            byteNumbers[i] = slice.charCodeAt(i);
+          }
+
+          const byteArray = new Uint8Array(byteNumbers);
+          byteArrays.push(byteArray);
+        }
+
+        const blob = new Blob(byteArrays, { type: contentType });
+        return blob;
+      };
+
+      const blob1 = b64toBlob(str, "image/png");
+      const blobUrl = URL.createObjectURL(blob1);
+      //console.log(blobUrl);
+      //window.location = blobUrl;
+      document.querySelector("#captchaWrapper").innerHTML="";
+      const captchaImg = document.createElement("img");
+      captchaImg.src = blobUrl;
+      document.querySelector("#captchaWrapper").appendChild(captchaImg);
+      // do whatever you want if console is [object object] then stringify the response
+    });
+};
+const uuidv4=()=> {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+  }
+  
+  console.log(uuidv4());
+const generateOTP =()=>
+{
+    axios.post('https://stage1.uidai.gov.in/unifiedAppAuthService/api/v2/generate/aadhaar/otp',
+    {
+        "x-request-id":uuidv4(),
+        "appid":"MYAADHAAR",
+        "Accept-Language":"en_in",
+        "Content-Type": "application/json "
+
+    })
+      .then((response) => {
+        console.log(response);
+      }, (error) => {
+        console.log(error);
+      });
+}
+createCaptcha();
